f86c7fc48957b45991747a584aefe3cfd04474ed
Usama Ali <usama.bscsf19@iba-suk.edu.pk>
2023-11-08 15:16:03 +0500

    Navbar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import HeroSection from "./components/HeroSection";
-
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 function App() {
